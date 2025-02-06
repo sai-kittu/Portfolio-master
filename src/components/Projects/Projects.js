aec0d035df82586332a8fa5757a4b0de53f4676e
kittu64 <saikrishnar64@gmail.com>
2025-02-06 11:51:15 -0500

    made required changes

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -26,7 +26,7 @@ function Projects() {
               isBlog={false}
               title="3D-Image"
               description="This 3D image depicts a high-fidelity isometric view of a futuristic urban environment. The scene features meticulously detailed architectural elements with a symmetrical design and sleek metallic finishes. Realistic textures and soft ambient lighting enhance the visual appeal, while shadows and reflections contribute to a dynamic sense of depth."
-              ghLink="https://github.com/sai-kittu/3D-Boxes"
+              // ghLink="https://github.com/sai-kittu/3D-Boxes"
               demoLink=" https://sai-kittu.github.io/3D-Boxes/"
             />
           </Col>
@@ -37,7 +37,7 @@ function Projects() {
               isBlog={false}
               title="Password-Generator"
               description="The tool ensures the generation of unpredictable passwords, making it difficult for attackers to guess or brute-force. It can be integrated into web applications, desktop utilities, or browser extensions to help users protect sensitive information."
-              ghLink="https://github.com/sai-kittu/password-generator"
+              //ghLink="https://github.com/sai-kittu/password-generator"
               demoLink="https://sai-kittu.github.io/password-generator/"
             />
           </Col>
@@ -48,7 +48,7 @@ function Projects() {
               isBlog={false}
               title="Quiz-App"
               description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/sai-kittu/Quiz-App.git"
+              // ghLink="https://github.com/sai-kittu/Quiz-App.git"
               demoLink=" https://sai-kittu.github.io/Quiz-App/"
             />
           </Col>
@@ -59,7 +59,7 @@ function Projects() {
               isBlog={false}
               title="Backgoround Changer"
               description="Background changes refer to the alteration of a website or application’s visual backdrop, often involving color, images, or patterns. They enhance the user experience by creating a visually appealing environment that aligns with the brand's identity. Dynamic background changes can also convey themes, moods, or responses to user interactions. Effective use of background changes improves aesthetics and user engagement while maintaining readability."
-              ghLink="https://github.com/sai-kittu/Background-Slider.git"
+              // ghLink="https://github.com/sai-kittu/Background-Slider.git"
               demoLink="https://sai-kittu.github.io/Background-Slider/"
             />
           </Col>
@@ -70,7 +70,7 @@ function Projects() {
               isBlog={false}
               title="To Do List"
               description="A To-Do List is a simple tool used to organize tasks by listing them in order of priority. It helps users keep track of daily activities, deadlines, and goals. Tasks can be marked as complete once finished, providing a sense of accomplishment. To-Do Lists improve productivity, reduce stress, and ensure important tasks are not forgotten."
-              ghLink="https://github.com/sai-kittu/TO-DO-LIST.git"
+              // ghLink="https://github.com/sai-kittu/TO-DO-LIST.git"
               demoLink="https://sai-kittu.github.io/TO-DO-LIST/"
             />
           </Col>
